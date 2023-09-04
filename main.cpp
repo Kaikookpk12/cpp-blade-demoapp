85df2c8ebe1fc6319adad718d9999f33a41bc673
Ihor Dubii <ihor.dubii@unibrix.com>
2023-09-04 18:25:27 +0200

    add visitor id

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <garydu.h>
+#include <visitorId.h>
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Get fingerprint demo" << std::endl;
-
     Gary::Du::sayHello();
+
+    Gary::VisitorId visitorId("finger print public key");
+   
+    std::cout << "Get fingerprint demo: " << visitorId.getVisitorId() << std::endl;
 }
